b7b1fa6abcb4c732d8f287fc2b0028eb6a18ae65
Xu Qi <helloassemble@gmail.com>
2016-12-15 13:59:23 +0800

    add which.rb

---
+++ ruby/which.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Cross-platform way of finding an executable in the $PATH.
+#
+#   which('ruby') #=> /usr/bin/ruby
+# http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable?(exe) && !File.directory?(exe)
+    }
+  end
+  return nil
+end
+
+p which 'which'
